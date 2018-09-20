c64eaaf36af701a0eab1099626b7003c96a162e7
devjaq <devjaq@gmail.com>
2018-09-20 18:03:00 -0400

    if you type in the textarea and hit the button, text is separated by the spaces into words

--- script.js
+++ script.js
@@ -3,7 +3,40 @@
 let textBox = document.getElementsByTagName("textarea");
 
 function buttonClick() {
+  let counter = 1;
   // console.log(textBox);
 console.log(textBox[0].value);
+let codedMsg = textBox[0].value;
+let word = [];
+let fullText = [];
+for (let i = 0; i < codedMsg.length; i++) {
+  // console.log( codedMsg.length);
+  // console.log(i);
+  // if (i === 1) {
+    
+  // }
+  
+  if (codedMsg[i] === " ") {
+    console.log("space");
+    console.log(`Start word ${counter += 1}`);
+    fullText.push(word);
+    word = [];
+  } else if (i === codedMsg.length-1) {
+    console.log(codedMsg[i]);
+    word.push(codedMsg[i]);
+    console.log(word);
+    console.log("end reached");
+    fullText.push(word);
+    word = [];
+  } else {
+    console.log(codedMsg[i]);
+    word.push(codedMsg[i]);
+    console.log(word);
+  }
+  
+}
+console.log(fullText);
+
+
 };
 
