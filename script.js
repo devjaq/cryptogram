5fcfb4e18f5c2a37657a522a2af98a943fb885c4
devjaq <devjaq@gmail.com>
2018-09-22 22:13:39 -0400

    main functionality complete. letter replacement working without overwriting unintended letters

--- script.js
+++ script.js
@@ -2,37 +2,55 @@
 
 let textBox = document.getElementsByTagName("textarea");
 
-let letterA = document.getElementById("a");
-// let letterA = document.getElementsByTagName("input");
-console.log(letterA);
-
-let fullText = "";
-// let workingCopy = "";
 let codedMsg = textBox[0].value;
-let workingCopy = textBox[0].value;
+let workingCopy = codedMsg.split("");
+console.log(workingCopy);
+
+
 
 let results = document.createElement("p");
-results.innerText = workingCopy;
+let newResult = workingCopy.join('');
+results.innerText = newResult;
 document.body.appendChild(results);
 
+let letterBox = document.createElement("section");
+document.body.appendChild(letterBox);
+let alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+for (let i = 0; i < 26; i++) {
+  let letterInput = document.createElement("div");
+  letterInput.innerHTML = `${alpha[i]} -> <input type="text" id="${alpha[i]}" newletter="${alpha[i]}" onblur="letterChange(id, value)">`
+  letterBox.appendChild(letterInput);
+
+}
+
+////// after you have removed focus on one of the letter inputs, this function happens
 let letterChange = (oldLetter, newLetter) => {
-  console.log(oldLetter);
-  console.log(newLetter);
+  let letter = document.getElementById(oldLetter);
+
+  if (newLetter === "" || newLetter === " ") {
+    console.log("Please enter a letter");
+  } else {
+    console.log(codedMsg);
+    
+    for (let i = 0; i < codedMsg.length; i++) {
+      if (codedMsg[i] === oldLetter && newLetter !== null) {
+        
+        // let newCopy = workingCopy.replace(oldLetter, newLetter);
+        workingCopy.splice(i, 1, newLetter);
+        
   
-  
-  for (let i = 0; i < codedMsg.length; i++) {
-    if (codedMsg[i] === oldLetter) {
-      console.log(i);
-      console.log(workingCopy[i]);
-      
-      let newCopy = workingCopy.replace(/a/g, newLetter);
-      console.log(newCopy);
-      workingCopy = newCopy;
-      console.log(workingCopy);
-      results.innerText = workingCopy;
-      
+        // letter.attributes("newletter") = newLetter;
+        letter.setAttribute("newletter", newLetter);
+        // letter.newletter = newLetter;
+        
+        
+        
+      }
     }
+    newResult = workingCopy.join('');
+    results.innerText = newResult;
   }
+  
 }
 
 function buttonClick() {
